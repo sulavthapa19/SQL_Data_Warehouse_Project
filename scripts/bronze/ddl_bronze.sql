6a4a15ecc019bba87590682387d748aa84f24e11
Sulav Thapa <54358760+sulavthapa19@users.noreply.github.com>
2025-12-20 12:54:23 +0200

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,79 @@
+/*
+============================================================================================================================
+DDL Script: Create Bronze Tables
+============================================================================================================================
+
+Script Purpose:
+    - Generates tables within the 'bronze' schema.
+    - Removes existing versions of Bronze tables if they already exist.
+    - Used to refresh or recreate DDL definitions for all Bronze tables.
+
+Warning:
+    Running this script will permanently remove all existing data
+    in the Bronze tables.
+
+Usage:
+    This script should be executed before any data loads, as it creates the Bronze tables required for storing raw source data.
+================================================================================================================================
+*/
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info (
+    cst_id               INT,
+    cst_key              NVARCHAR(50),
+    cst_firstname        NVARCHAR(50),
+    cst_lastname         NVARCHAR(50),
+    cst_marital_status   NVARCHAR(50),
+    cst_gndr             NVARCHAR(50),
+    cst_create_date      DATE
+);
+
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info (
+    prd_id        INT,
+    prd_key       NVARCHAR(50),
+    prd_nm        NVARCHAR(50),
+    prd_cost      INT,
+    prd_line      NVARCHAR(50),
+    prd_start_dt  DATETIME,
+    prd_end_dt    DATETIME
+);
+
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num   NVARCHAR(50),
+    sls_prd_key   NVARCHAR(50),
+    sls_cust_id   INT,
+    sls_order_dt  INT,
+    sls_ship_dt   INT,
+    sls_due_dt    INT,
+    sls_sales     INT,
+    sls_quantity  INT,
+    sls_price     INT
+);
+
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101 (
+    CID    NVARCHAR(50),
+    CNTRY  NVARCHAR(50)
+);
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12 (
+    CID    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2 ', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    ID           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
